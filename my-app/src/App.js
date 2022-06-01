9fac0b14647ade283bb9a98ebe705fc7ef7611f5
Yinusa <yinusaamusa137@gmail.com>
2022-05-31 23:48:02 -0500

    learned new v6 syntax

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,30 +1,25 @@
-import React, { useEffect } from "react";
-import ReactDOM from "react-dom";
-import {BrowserRouter,Route,Link,Routes} from "react-router-dom";
+import React from "react";
+// import ReactDOM from "react-dom";
+import {Route,Link,Routes} from "react-router-dom";
+import Home from "./components/Home.js";
+import About from "./components/About.js";
+import Contact from "./components/contact.js";
 import './App.css';
 
 
-function Home(props){
-  return <h1>Home</h1>
-}
-function About(props){
-  return <h1>About</h1>
-}
 
-function Contact(props){
-  return <h1>Contact</h1>
-}
 
-function hist(props){
-  const {push} = props.history;
-  return <button onClick={() => push("/")}>Back Home</button>
-  }
 
- const Project = props => {
-    const {id} = props.match.params;
-    const project = "placeholder" //find(proj=>proj.id == id)
-    return <div> The id is {id} </div>
-  }
+// function hist(props){
+//   const {push} = props.history;
+//   return <button onClick={() => push("/")}>Back Home</button>
+//   }
+
+//  const Project = props => {
+//     const {id} = props.match.params;
+//     const project = "placeholder" //find(proj=>proj.id == id)
+//     return <div> The id is {id} </div>
+//   }
 
   // useEffect(() => {
   //side effect run for projects api
@@ -33,31 +28,32 @@ function hist(props){
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       <BrowserRouter> 
-        <Link to='/'>Home</Link>  {/* pathname,search,hash, state:{from} */}
-        <Link to='/about'>About </Link>
-        <Link to='/contact'>Contact</Link>
+      <div>
+      <header className="App-header"> 
+        <Link to="/">Home </Link>  {/* pathname,search,hash, state:{from} */}
+        <Link to="about">About </Link>
+        <Link to="/contact">Contact</Link>
         <Link to="/project/1"></Link>
+        
+
         <Routes>
-        <Route exact path='/' component={Home}/>
-        <Route path='/about' component={About}/>
-        <Route path='/contact' component={Contact}/>
+        <Route path='/' element={<Home/>}/>      {/* NOTE: IN V6 react-router component is deprecated, use element */}
+        <Route path='about' element={<About />}/>
+        <Route path='/contact' element={<Contact />}/>
+        </Routes>
 
          
          {/*<Route path='/project/:id' component={Project} />*/}
 
-        </Routes>
-       </BrowserRouter>
       </header>
-      <div>
+      </div>
+     {/* <body>
       <h1>Hi, my name is Z and I'm a full stack web developer based in Chicago, IL. 
         I have a passion for designing and working with API's and making the impossible possible. 
         I'm open to working with a great team of developers who love creating state-of-the-art solutions that make a difference</h1>
-        </div>
+        </body> */}
 
-        <footer>
-        <BrowserRouter> 
+        <footer className="footerA">
         <Link to='/'>Home</Link>  {/* pathname,search,hash, state:{from} */}
         <Link to='/about'>About </Link>
         <Link to='/contact'>Contact</Link>
@@ -71,7 +67,6 @@ function App() {
          {/*<Route path='/project/:id' component={Project} />*/}
 
         </Routes>
-       </BrowserRouter>
         </footer>
     </div>
   );
